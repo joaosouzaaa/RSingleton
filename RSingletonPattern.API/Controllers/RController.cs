211b81fb94afef600b5cd417bfc7f11e38cf8376
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-08 19:19:00 -0300

    Feat: Adding RController.

---
+++ RSingletonPattern.API/Controllers/RController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSingletonPattern.API.Arguments;
+using RSingletonPattern.API.Interfaces.Services;
+
+namespace RSingletonPattern.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class RController(IRService rService) : ControllerBase
+{
+    [HttpPost("sum-numbers")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public int SumTwoNumbers([FromBody] SumTwoNumbers sum) =>
+        rService.SumTwoNumbers(sum);
+}
