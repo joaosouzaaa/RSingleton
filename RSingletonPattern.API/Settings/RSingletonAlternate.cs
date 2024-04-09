37555c445a656b090e36b88ee0f183c4e4185c87
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-09 14:30:08 -0300

    Feat: Implementing Alternate way of singleton.

---
+++ RSingletonPattern.API/Settings/RSingletonAlternate.cs
@@ -0,0 +1,21 @@
+﻿using RDotNet;
+
+namespace RSingletonPattern.API.Settings;
+
+public sealed class RSingletonAlternate
+{
+    private static REngine? _engine;
+
+    private RSingletonAlternate()
+    {
+        
+    }
+
+    public static REngine GetInstance()
+    {
+        if(_engine is null)
+            _engine = REngine.GetInstance();
+
+        return _engine;
+    }
+}
