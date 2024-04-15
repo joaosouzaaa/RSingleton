1f4c6e78785fe74b668c3dceb55bcd315fdb2100
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-15 17:54:42 -0300

    Fix: Making RSingleton Thread-Safe.

--- RSingletonPattern.API/Settings/RSingleton.cs
+++ RSingletonPattern.API/Settings/RSingleton.cs
@@ -4,8 +4,19 @@ namespace RSingletonPattern.API.Settings;
 
 public static class RSingleton
 {
-    private static readonly REngine _engine = REngine.GetInstance();
+    private static readonly object _lock = new();
+    private static REngine? _engine;
 
-    public static REngine GetInstance() =>
-        _engine;
+    public static REngine GetInstance()
+    {
+        lock (_lock)
+        {
+            if (_engine is null)
+            {
+                _engine = REngine.GetInstance();
+            }
+        }
+
+        return _engine;
+    }
 }
